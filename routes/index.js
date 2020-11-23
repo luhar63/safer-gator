85eeb63c8137a6464fc01b463643e8a4b6e1b02e
Rahul Maurya <rahulmaurya.mnnit@gmail.com>
2020-11-23 04:52:14 -0500

    updated map with different metric data

--- routes/index.js
+++ routes/index.js
@@ -19,12 +19,85 @@ router.get('/zone', async (req, res) => {
 
 
 });
+
+
+router.get('/blp', async (req, res) => {
+
+    // let rawdata = fs.readFileSync('./json/search.json');
+
+    // let search = JSON.parse(rawdata);
+    fs.readFile(path.resolve(__dirname, "../json/blp.json"), function (err, data) {
+
+        // Check for errors 
+        if (err) throw err;
+
+        // Converting to JSON 
+        let blp = JSON.parse(data);
+        res.send(blp);
+    });
+
+
+});
+
+
+router.get('/search', async (req, res) => {
+
+    // let rawdata = fs.readFileSync('./json/search.json');
+
+    // let search = JSON.parse(rawdata);
+    fs.readFile(path.resolve(__dirname, "../json/search.json"), function (err, data) {
+
+        // Check for errors 
+        if (err) throw err;
+
+        // Converting to JSON 
+        let search = JSON.parse(data);
+        res.send(search);
+    });
+
+
+});
+
+router.get('/bus_stops', async (req, res) => {
+
+    // let rawdata = fs.readFileSync('./json/search.json');
+
+    // let search = JSON.parse(rawdata);
+    fs.readFile(path.resolve(__dirname, "../json/bus_stops.json"), function (err, data) {
+
+        // Check for errors 
+        if (err) throw err;
+
+        // Converting to JSON 
+        let search = JSON.parse(data);
+        res.send(search);
+    });
+
+
+});
 router.get('/buildings', async (req, res) => {
 
     // let rawdata = fs.readFileSync('./json/search.json');
 
     // let search = JSON.parse(rawdata);
     fs.readFile(path.resolve(__dirname, "../json/geo_buildings.json"), function (err, data) {
+
+        // Check for errors 
+        if (err) throw err;
+
+        // Converting to JSON 
+        let building = JSON.parse(data);
+        res.send(building);
+    });
+
+
+});
+router.get('/crimes', async (req, res) => {
+
+    // let rawdata = fs.readFileSync('./json/search.json');
+
+    // let search = JSON.parse(rawdata);
+    fs.readFile(path.resolve(__dirname, "../json/crimes_locations.json"), function (err, data) {
 
         // Check for errors 
         if (err) throw err;
